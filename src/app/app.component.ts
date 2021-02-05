adf22e40073f980e60c636660ae6fa26036007bd
venki <tvc103@gmail.com>
2021-02-05 16:41:00 +0530

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,92 +1,144 @@
-import { Component,ViewChild, AfterViewInit, ElementRef, OnInit } from '@angular/core';
+import {
+  Component,
+  ViewChild,
+  AfterViewInit,
+  ElementRef,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  implements OnInit,AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  firstdelete: boolean = false;
+  seconddelete: boolean = false;
+  thirddelete: boolean = false;
+  fourthdelete: boolean = false;
+  fifthdelete: boolean = false;
+  sixdelete: boolean = false;
 
+  ngOnInit() {}
 
-  ngOnInit(){
-
-    if(this.sixthdeleteicon){
-      this.sixthdeleteicon.addEventListener('click',()=>{
-        debugger;
-        console.log("ghhh")
-      })
-      }
-
-  }
+  constructor(private render: Renderer2) {}
 
   // firstimg="../../assets/inputoutput.jpg"
-  sixthdeleteicon:any;
 
-@ViewChild("first",{static:false}) first:ElementRef
-@ViewChild("second",{static:false}) second:ElementRef
-@ViewChild("third",{static:false}) third:ElementRef
-@ViewChild("fourth",{static:false}) fourth:ElementRef
-@ViewChild("fifth",{static:false}) fifth:ElementRef
-@ViewChild("sixth",{static:false}) sixth:ElementRef
- 
+  @ViewChild('first', { static: false }) first: ElementRef;
+  @ViewChild('second', { static: false }) second: ElementRef;
+  @ViewChild('third', { static: false }) third: ElementRef;
+  @ViewChild('fourth', { static: false }) fourth: ElementRef;
+  @ViewChild('fifth', { static: false }) fifth: ElementRef;
+  @ViewChild('sixth', { static: false }) sixth: ElementRef;
+
   ngAfterViewInit() {
+    // ****************************first & second clicks**********************************************//
+    this.first.nativeElement.addEventListener('click', () => {
+      this.render.addClass(this.first.nativeElement, 'firstwelcome');
+      this.firstdelete = true;
+    });
 
-   
+    this.second.nativeElement.addEventListener('click', () => {
+      this.second.nativeElement.classList.add('secondwelcome');
+      this.seconddelete = true;
+    });
 
-  
-  
-// ****************************first & second clicks**********************************************//
-this.first.nativeElement.addEventListener('click',()=>{
-  this.first.nativeElement.classList.add('welcome');
-  this.first.nativeElement.innerHTML=`<i id="firsticon" class="fa fa-trash fa-3x" aria-hidden="true"></i>`
-})
+    // ****************************first & second clicks**********************************************//
 
-this.second.nativeElement.addEventListener('click',()=>{
-  this.second.nativeElement.classList.add('welcome');
-  this.second.nativeElement.innerHTML=`<i id="firsticon" class="fa fa-trash fa-3x" aria-hidden="true"></i>`
-})
+    // $$$$$$$$$$$$$$$$$$$$$$$$$$$ third & fourth Button clicks $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
 
-// ****************************first & second clicks**********************************************//
+    this.third.nativeElement.addEventListener('click', () => {
+      this.third.nativeElement.classList.add('thirdwelcome');
+      this.thirddelete = true;
+    });
+    this.fourth.nativeElement.addEventListener('click', () => {
+      this.fourth.nativeElement.classList.add('fourthwelcome');
+      this.fourthdelete = true;
+    });
+    // $$$$$$$$$$$$$$$$$$$$$$$$$$$ third & fourth Button clicks $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
 
-// $$$$$$$$$$$$$$$$$$$$$$$$$$$ third & fourth Button clicks $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
-
-this.third.nativeElement.addEventListener('click',()=>{
-  
-  this.third.nativeElement.classList.add('welcome');
-  this.third.nativeElement.innerHTML=`<i id="firsticon" class="fa fa-trash fa-3x" aria-hidden="true"></i>`
-})
-this.fourth.nativeElement.addEventListener('click',()=>{
-  this.fourth.nativeElement.classList.add('welcome');
-  this.fourth.nativeElement.innerHTML=`<i id="firsticon" class="fa fa-trash fa-3x" aria-hidden="true"></i>`
-})
-// $$$$$$$$$$$$$$$$$$$$$$$$$$$ third & fourth Button clicks $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$//
-
-
-this.fifth.nativeElement.addEventListener('click',()=>{
-  // this.fifth.nativeElement.innerHTML=`<img src="${this.firstimg}" height="200" width="200">
-  // <i id="fifthicon" class="fa fa-trash fa-3x" aria-hidden="true"></i>`
-
-  this.fifth.nativeElement.classList.add('welcome');
-  this.fifth.nativeElement.innerHTML=`<i id="firsticon" class="fa fa-trash fa-3x" aria-hidden="true"></i>`
-})
-this.sixth.nativeElement.addEventListener('click',()=>{
-  this.sixth.nativeElement.classList.add('welcome');
-  this.sixth.nativeElement.innerHTML=`<i id="firsticon" class="fa fa-trash fa-3x" aria-hidden="true"></i>`
-  this.sixthdeleteicon=this.sixth.nativeElement.childNodes[0]
-
-})
-
-
-
-
-
-
+    this.fifth.nativeElement.addEventListener('click', () => {
+      this.fifth.nativeElement.classList.add('fifthwelcome');
+      this.fifthdelete = true;
+    });
+    this.sixth.nativeElement.addEventListener('click', () => {
+      this.sixth.nativeElement.classList.add('sixthwelcome');
+      this.sixdelete = true;
+    });
   }
 
-
-
-  
- 
-
-
+  deleteclick(data) {
+    if (data == 'first') {
+      setTimeout(() => {
+        this.render.removeClass(this.first.nativeElement, 'firstwelcome');
+        this.firstdelete = false;
+      }, 500);
+    } else if (data == 'second') {
+      if (
+        this.first.nativeElement.classList.length == 1 &&
+        this.second.nativeElement.classList.length == 1 &&
+        this.third.nativeElement.classList.length == 1 &&
+        this.fourth.nativeElement.classList.length == 1 &&
+        this.fifth.nativeElement.classList.length == 1 &&
+        this.sixth.nativeElement.classList.length == 1
+      ) {
+        setTimeout(() => {
+          this.second.nativeElement.remove();
+          this.render.removeClass(this.third.nativeElement, 'thirdwelcome');
+          this.third.nativeElement.classList.add('fourthwelcome');
+          this.render.removeClass(this.fourth.nativeElement, 'fourthwelcome');
+          this.fourth.nativeElement.classList.add('fifthwelcome');
+          this.render.removeClass(this.fifth.nativeElement, 'fifthwelcome');
+          this.fifth.nativeElement.classList.add('sixthwelcome');
+          this.render.removeClass(this.sixth.nativeElement, 'sixthwelcome');
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          this.render.removeClass(this.second.nativeElement, 'secondwelcome');
+          this.seconddelete = false;
+        }, 1000);
+      }
+    } else if (data == 'third') {
+      setTimeout(() => {
+        this.render.removeClass(this.third.nativeElement, 'thirdwelcome');
+        this.thirddelete = false;
+      }, 1000);
+    } else if (data == 'fourth') {
+      setTimeout(() => {
+        this.render.removeClass(this.fourth.nativeElement, 'fourthwelcome');
+        this.fourthdelete = false;
+      }, 1000);
+    } else if (data == 'fifth') {
+      setTimeout(() => {
+        this.render.removeClass(this.fifth.nativeElement, 'fifthwelcome');
+        this.fifthdelete = false;
+      }, 1000);
+    } else if (data == 'sixth') {
+      if (
+        this.fifth.nativeElement.classList.length == 1 ||
+        this.fourth.nativeElement.classList.length == 1 ||
+        this.third.nativeElement.classList.length == 1
+      ) {
+        setTimeout(() => {
+          this.render.removeClass(this.third.nativeElement, 'thirdwelcome');
+          this.thirddelete = false;
+          this.render.removeClass(this.fourth.nativeElement, 'fourthwelcome');
+          this.fourthdelete = false;
+          this.render.removeClass(this.fifth.nativeElement, 'fifthwelcome');
+          this.fifthdelete = false;
+          this.render.removeClass(this.sixth.nativeElement, 'sixthwelcome');
+          this.sixdelete = false;
+          this.third.nativeElement.classList.add('thirdwelcome');
+          this.thirddelete = true;
+        }, 500);
+      } else {
+        setTimeout(() => {
+          this.render.removeClass(this.sixth.nativeElement, 'sixthwelcome');
+          this.sixdelete = false;
+        }, 1000);
+      }
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,8 @@
+import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
+
 
 @NgModule({
   declarations: [
